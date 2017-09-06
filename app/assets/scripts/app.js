9a239fc0076f93ced38a3c561f7d3ec6c61a4161
Kyle Grenell <kyle.grenell@gmail.com>
2017-09-06 21:15:38 +0100

    transition header logo and darken navbar added

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import disMobileThang from './modules/mobileMenu';
 import RevealOnScroll from './modules/revealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 let mobileMenu = new disMobileThang();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+let stickyHeader = new StickyHeader();
